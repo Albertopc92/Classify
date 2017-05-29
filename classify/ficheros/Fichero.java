1bae4a23c687b7eeefdeb71e4fea08c1b5124da6
Albertopc92 <albertopc92@outlook.com>
2017-05-29 21:07:28 +0200

    Cambio de carpeta ficheros

--- classify/ficheros/Fichero.java
+++ classify/ficheros/Fichero.java
@@ -1,4 +1,4 @@
-package ficheros;
+package classify.ficheros;
 
 import java.io.File;
 import java.io.FileInputStream;
--- classify/ficheros/Filtro.java
+++ classify/ficheros/Filtro.java
@@ -1,4 +1,4 @@
-package ficheros;
+package classify.ficheros;
 
 import java.io.File;
 import javax.swing.filechooser.FileFilter;
